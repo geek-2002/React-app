fac49e1a940608597a4b7f716fc147561f4c43b0
Sankirtana <59257797+geek-2002@users.noreply.github.com>
2020-08-05 13:50:06 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Note from "./Note.jsx";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteItem => {
+        return (
+          <Note
+            key={noteItem.key}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
 }
+
 export default App;
